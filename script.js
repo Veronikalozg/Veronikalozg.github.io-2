c6f7171b1016692ff4beb27f133297286523a1ad
Veronikalozg <113824431+Veronikalozg@users.noreply.github.com>
2022-10-09 15:38:38 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+$('.slider').slick({
+	infinite: true,
+	slidesToShow: 4,
+	slidesToScroll: 3,
+  	responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 580,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  	]
+}); 
